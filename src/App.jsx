8c04bb0bb3b23e6ed71ddba694590e3fc90a4446
Kiran <km060360@gmail.com>
2024-03-22 21:40:51 +0530

    box comp added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import React from "react";
-import Base from "./components/Base";
-import Foreground from "./components/Foreground";
-
+import React from 'react';
+import Base from './components/Base';
+import Foreground from './components/Foreground';
+import Box from './components/Box';
 
 function App() {
-  return (
-    <div className="relative w-full h-screen bg-[#08090A]">    
-      {/* <div className="abosolute">
+	return (
+		<div className='relative w-full h-screen bg-[#08090A]'>
+			{/* <div className="abosolute">
         <Base />
       </div> */}
-      <Foreground />
-    </div>
-  );
+			<Foreground />
+			<Box />
+		</div>
+	);
 }
 
 export default App;
