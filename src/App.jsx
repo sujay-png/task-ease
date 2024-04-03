9b4ff5d07a5e8287506ccad417a042aee964e44e
sujay.png <sujaykumarp00@gmail.com>
2024-04-03 15:58:42 +0530

    added drag feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import Base from "./components/Base";
-import Foreground from "./components/Foreground";
-
-function App() {
-  return (
-    <div className="relative w-full h-screen bg-[#08090A]">
-      <div className="abosolute">
-        <Base />
-      </div>
-      <Foreground />
-    </div>
-  );
-=======
 import React from 'react';
 import Base from './components/Base';
 import Foreground from './components/Foreground';
@@ -21,15 +6,14 @@ import Box from './components/Box';
 function App() {
 	return (
 		<div className='relative w-full h-screen bg-[#08090A]'>
-			{/* <div className="abosolute">
+			<div className="abosolute">
         <Base />
-      </div> */}
+      </div>
       hi
 			<Foreground />
 			<Box />
 		</div>
 	);
->>>>>>> 36591487039272d410076f44f5825cf18b2f2a91
 }
 
 export default App;
